fe1128d33dc1975ec23f084674c3833e83247d0d
horabets <164284668+horabets@users.noreply.github.com>
2024-05-24 01:44:02 -0300

    Create img.js

---
+++ img.js
@@ -0,0 +1,11 @@
+// facebook-pixel.js
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1208877257150737'); 
+fbq('track', 'PageView');
